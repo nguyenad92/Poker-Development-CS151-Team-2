f0c698eded07412bcb77499fcccaab1c3b7a2edb
Calvin Nguyen <cuongvqnguyen@gmail.com>
2018-10-04 15:32:52 -0700

    add constructor and basic function for each class

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -1,13 +1,32 @@
+import java.util.ArrayList;
+
 /**
  * This PokerGame class will be the main loop to generate the game
  */
 public class PokerGame {
+
+    private ArrayList<Player> playerList = new ArrayList<>();
+    private Dealer dealer = new Dealer();
+    private DeckOfCard deckOfCard = new DeckOfCard();
+
 
     /**
      * Main Function will run the whole program
      * @param args
      */
     public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public void addPlayer(Player p) {
+
+    }
+
+    public void addDealer(Dealer d) {
+
+    }
+
+    public void resetGame(){
 
     }
 }
