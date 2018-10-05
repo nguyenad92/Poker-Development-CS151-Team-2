fc9b009e0e7829f6dffec96d9d521deee1536b5f
ADNguyen <nguyenad92@gmail.com>
2018-10-05 00:02:19 -0700

    First try

--- src/Player.java
+++ src/Player.java
@@ -5,4 +5,5 @@
  */
 public class Player {
 	private int age;
+
 }
--- src/PokerGame.java
+++ src/PokerGame.java
@@ -8,6 +8,6 @@ public class PokerGame {
      * @param args
      */
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
