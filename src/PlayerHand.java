1913479f4b8550dd153666abc34e00b9bf7b6ab8
tyninhung <dbsk0307>
2018-11-08 08:55:33 -0800

    Added Player object & PlayerHand object

--- src/PlayerHand.java
+++ src/PlayerHand.java
@@ -1,7 +1,35 @@
+import java.util.Set;
+
 /**
  * A class to keep cards of players
  *
  */
 public class PlayerHand {
+    private Player player;
+    Set<Card> cards;
+
+    /**
+     * Construct a playerHand object
+     */
+    public PlayerHand(Player player, Set<Card> cards) {
+        this.player = player;
+        this.cards = cards;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Set<Card> getCard() {
+        return cards;
+    }
+
+    public void playerHand() {
+
+    }
+
+    public int compareRankHand() {
+        return 0;
+    }
 
 }
